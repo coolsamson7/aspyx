ae84613db4e87f0cf2e0edf9bb78cb1d6f637975
Andreas Ernst <andreas.ernst7@gmail.com>
2025-06-26 16:33:23 +0200

    script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
-cd packages/aspyx && hatch build
-cd ../aspyx_service && hatch build
+
+echo "### build aspyx"
+cd packages/aspyx
+hatch build
+
+echo "### build aspyx_service"
+ls ../aspyx_service
+cd ../aspyx_service
+hatch build
