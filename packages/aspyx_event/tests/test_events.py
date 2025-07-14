831aca054b0a4c0321fa2c5c2ab9c4b294291970
Andreas Ernst <andreas.ernst7@gmail.com>
2025-07-14 10:47:16 +0200

    refactored test provider

--- packages/aspyx_event/tests/test_events.py
+++ packages/aspyx_event/tests/test_events.py
@@ -2,13 +2,12 @@
 test for health checks
 """
 from __future__ import annotations
-
-import asyncio
 import logging
 import threading
-from typing import Optional
 
 from aspyx.util import Logger
+
+from .provider import LocalProvider
 
 Logger.configure(default_level=logging.INFO, levels={
     "httpx": logging.ERROR,
@@ -27,9 +26,10 @@ from dataclasses import dataclass
 import pytest
 
 from aspyx_event import EventManager, event, envelope_pipeline, AbstractEnvelopePipeline, \
-    event_listener, EventListener, StompProvider, AMQPProvider
+    event_listener, EventListener, EventModule
+#StompProvider, AMQPProvider
 
-from aspyx.di import module, Environment, create, inject_environment
+from aspyx.di import module, Environment, create
 
 
 # test classes
@@ -90,77 +90,25 @@ class OtherHelloEventListener(EventListener[HelloEvent]):
         logger.info("other listen to hello " + event.hello)
         #event_received.set()
 
-class TestProvider(EventManager.Provider):
-    # local classes
-
-    class TestEnvelope(EventManager.Envelope):
-        # constructor
-
-        def __init__(self, body="", headers=None):
-            self.body = body
-            self.headers = headers or {}
-
-        # implement envelope
-
-        def get_body(self) -> str:
-            return self.body
-
-        def set(self, key: str, value: str):
-            self.headers[key] = value
-
-        def get(self, key: str) -> str:
-            return self.headers.get(key,"")
-
-    # constructor
-
-    def __init__(self):
-        super().__init__()
-
-        self.environment : Optional[Environment] = None
-        self.listeners : list[EventManager.EventListenerDescriptor] = []
-
-    # inject
-
-    @inject_environment()
-    def set_environment(self, environment: Environment):
-        self.environment = environment
-
-    # implement Provider
-
-    def create_envelope(self, body="", headers = None) -> EventManager.Envelope:
-        return TestProvider.TestEnvelope(body=body, headers=headers)
-
-    def listen_to(self, listener: EventManager.EventListenerDescriptor) -> None:
-        self.listeners.append(listener)
-
-    # implement EnvelopePipeline
-
-    def send(self, envelope: EventManager.Envelope, event_descriptor: EventManager.EventDescriptor):
-        #self.handle(envelope, event_descriptor)
-        self.manager.pipeline.handle(envelope, event_descriptor)
-
-    def handle(self, envelope: EventManager.Envelope, event_descriptor: EventManager.EventDescriptor):
-        for listener in self.listeners:
-            if listener.event is event_descriptor:
-                self.manager.dispatch_event(listener, envelope.get_body())
-
 # test module
 
-@module(imports=[])
+@module(imports=[EventModule])
 class Module:
     def __init__(self):
         pass
 
     @create()
     def create_event_manager(self) -> EventManager:
-        return EventManager(TestProvider())
+        return EventManager(LocalProvider())
         # EventManager(StompProvider(host="localhost", port=61616, user="artemis", password="artemis"))
         # EventManager(AMQPProvider("server-id", host="localhost", port=5672, user="artemis", password="artemis"))
 
 @pytest.fixture(scope="session")
 def environment():
     environment = Environment(Module)  # start server
+
     yield environment
+
     environment.destroy()
 
 class TestLocalService():
@@ -168,12 +116,5 @@ class TestLocalService():
         event_manager = environment.get(EventManager)
 
         event_manager.send_event(HelloEvent("world1"))
-
-        while False:
-            #print(">", flush=True)
-            logger.info(".")
-            event_manager.send_event(HelloEvent("WORLD"))
-            #print(".", flush=True)
-            #sleep(1)
 
         assert event_received.wait(timeout=100), "ouch"
