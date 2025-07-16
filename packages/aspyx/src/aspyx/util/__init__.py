13c9ed125d9e5fe1b3d2a4101db64b5e8d83ae69
Andreas Ernst <andreas.ernst7@gmail.com>
2025-07-16 09:53:05 +0200

    serialization is part of aspyx core

--- packages/aspyx/src/aspyx/util/__init__.py
+++ packages/aspyx/src/aspyx/util/__init__.py
@@ -3,8 +3,14 @@ This module provides utility functions.
 """
 from .stringbuilder import StringBuilder
 from .logger import Logger
+from .serialization import TypeSerializer, TypeDeserializer, get_serializer, get_deserializer
 
 __all__ = [
     "StringBuilder",
-    "Logger"
+    "Logger",
+
+    "TypeSerializer",
+    "TypeDeserializer",
+    "get_serializer",
+    "get_deserializer"
 ]
