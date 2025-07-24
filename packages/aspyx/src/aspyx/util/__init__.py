1d383e827839e4cf4c5d1fed78b37376859ce5e7
Andreas Ernst <andreas.ernst7@gmail.com>
2025-07-24 22:28:01 +0200

    added copy on write cache

--- packages/aspyx/src/aspyx/util/__init__.py
+++ packages/aspyx/src/aspyx/util/__init__.py
@@ -4,10 +4,13 @@ This module provides utility functions.
 from .stringbuilder import StringBuilder
 from .logger import Logger
 from .serialization import TypeSerializer, TypeDeserializer, get_serializer, get_deserializer
+from .copy_on_write_cache import CopyOnWriteCache
 
 __all__ = [
     "StringBuilder",
     "Logger",
+
+    "CopyOnWriteCache",
 
     "TypeSerializer",
     "TypeDeserializer",
