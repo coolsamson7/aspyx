631e17863eb0f1b4798e8d06d1d2cd05448bbe2c
Andreas Ernst <andreas.ernst7@gmail.com>
2025-11-20 08:53:21 +0100

    job

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ROOT_DIR=$(pwd)
 ASPYX_PATH="$ROOT_DIR/packages/aspyx"
 ASPYX_SERVICE_PATH="$ROOT_DIR/packages/aspyx_service"
 ASPYX_EVENT_PATH="$ROOT_DIR/packages/aspyx_event"
+ASPYX_JOB_PATH="$ROOT_DIR/packages/aspyx_job"
+ASSAIA_PATH="$ROOT_DIR/packages/assaia"
 
 echo "Installing aspyx..."
 pip install -e "$ASPYX_PATH"
@@ -15,5 +17,8 @@ pip install -e "$ASPYX_SERVICE_PATH"
 
 echo "Installing aspyx_event..."
 pip install -e "$ASPYX_EVENT_PATH"
+
+echo "Installing aspyx_job..."
+pip install -e "$ASSAIA_JOB_PATH"
 
 echo "All done!"
